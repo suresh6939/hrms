e8969baabe353dd9b5b0e16714b76cde99f503d7
Trivikram7780 <trivikram2001@gmail.com>
2024-11-16 07:42:15 +0530

    hours

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,11 +1,36 @@
 import Header from '../Header'
+import { useState } from "react";
 import './index.css'
 
 const Home = () => {
+
+    const [homeariable ,setHomeVariable] = useState({
+        hours : 0,
+        minutes : 0,
+        checkIn:'08:00',
+        checkOut:'23:78'
+    })
+
     return(
      <div className="home-container">
         <Header />
-        <h1>hi</h1>
+        <div className='home-page-container'>
+            <div className='hours-checkin-checkout'>
+                 <label className='heading'>Today Total Hours</label>
+                 <div className='hours-minutes'>
+                   <label className='clock-para'>{homeariable.hours} : {homeariable.minutes}</label>
+                 </div>
+                 <button className='check-in'>
+                    Check In
+                 </button>
+                 <button className='check-in'>
+                    Check Out
+                 </button>
+                 <button className='check-in'>
+                    Reload
+                 </button>
+            </div>
+        </div>
      </div>
     )
 }
